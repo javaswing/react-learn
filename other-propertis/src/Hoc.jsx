185093575e5000c7539d6c399939c6b1fe67f559
javaSwing <zxdlovejava@gmail.com>
2020-12-20 23:48:33 +0800

    feat: update docs

--- other-propertis/src/Hoc.jsx
+++ other-propertis/src/Hoc.jsx
@@ -2,12 +2,19 @@ import React, { useRef, useEffect } from 'react';
 import withHeader from './withHeader';
 
 // 如果使用HOC组件需要使用到ref，那么需要把函数组件换成类组件才可以
+// 否则会提示：Function components cannot be given refs.
 class Demo extends React.Component {
   render () {
     const {geRef, ...props} = this.props
     return <div ref={geRef} {...props}>我是一个普通的组件</div>
   }
 }
+
+// 获取不到ref: Function components cannot be given refs.
+// function Demo(props) {
+//   const { getRef, ...rest } = props;
+//   return <div ref={getRef} {...rest}>我是一个普通的组件</div>
+// }
 
 
 const WidthHeaderDemo = withHeader(Demo, '自定义标题')
