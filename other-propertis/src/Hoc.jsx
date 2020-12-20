7ed302cd3a868fef1e4410cee29f985565611ffa
javaSwing <zxdlovejava@gmail.com>
2020-12-20 23:57:26 +0800

    feat: function forwardedRef

--- other-propertis/src/Hoc.jsx
+++ other-propertis/src/Hoc.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, forwardRef } from 'react';
 import withHeader from './withHeader';
 
 // 如果使用HOC组件需要使用到ref，那么需要把函数组件换成类组件才可以
@@ -10,14 +10,16 @@ class Demo extends React.Component {
   }
 }
 
+const WidthHeaderDemo = withHeader(Demo, '自定义标题')
+
 // 获取不到ref: Function components cannot be given refs.
-// function Demo(props) {
-//   const { getRef, ...rest } = props;
-//   return <div ref={getRef} {...rest}>我是一个普通的组件</div>
+// function Demo(props, ref) {
+//   return <div ref={ref} {...props}>我是一个普通的组件</div>
 // }
 
-
-const WidthHeaderDemo = withHeader(Demo, '自定义标题')
+// 这样写获取的是Demo中的ref
+// const FunctionDemo = forwardRef(Demo)
+// const WidthHeaderDemo = withHeader(FunctionDemo, '自定义标题')
 
 export default function Hoc(params) {
   const ref = useRef(null)
