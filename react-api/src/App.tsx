78d147a46f2c84621ee3281636c106ac9d646076
javaSwing <zxdlovejava@gmail.com>
2021-06-07 19:56:36 +0800

    feat: add react api

---
+++ react-api/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import PureComponents from "./components/pure-component";
+
+function App() {
+  return (
+    <div className="App">
+      <PureComponents />
+    </div>
+  );
+}
+
+export default App;
