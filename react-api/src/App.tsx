b210d35a07bf01472ca000bb7f7347e498f0bba6
javaSwing <zxdlovejava@gmail.com>
2021-06-07 22:39:14 +0800

    feat: add react-api dir

---
+++ react-api/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PureComponentDemo from "./components/pure";
+
+function App() {
+  return (
+    <div className="App">
+      <PureComponentDemo />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-api/src/components/pure/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+class PureComponentDemo extends React.PureComponent {}
+
+export default PureComponentDemo;
